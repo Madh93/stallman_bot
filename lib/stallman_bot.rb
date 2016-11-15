e89db197a97a7cc3c4a3683b99609f6cdf4daf1a
Madh93 <michelangelo93@gmail.com>
2016-11-15 21:59:01 +0000

    stallman executable

--- lib/stallman_bot.rb
+++ lib/stallman_bot.rb
@@ -1,5 +1,20 @@
 require 'stallman_bot/version'
 
 module StallmanBot
-  # Your code goes here...
+  class StallmanBot
+    attr_accessor :debug, :token
+
+    def initialize(opt = {})
+      if block_given?
+        yield self
+      else
+        @debug = opt[:debug]
+        @token = opt[:token]
+      end
+    end
+
+    def run
+      puts "bot corriendo"
+    end
+  end
 end
