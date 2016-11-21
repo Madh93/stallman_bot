8517990ba87e7cf8ff0d2fadfda3c205d29a61ca
Madh93 <michelangelo93@gmail.com>
2016-11-21 23:04:51 +0000

    call commands

--- lib/stallman_bot/commands.rb
+++ lib/stallman_bot/commands.rb
@@ -2,10 +2,25 @@
 
 module StallmanBot
   module Commands
+    def self.commands
+      path = File.join(File.dirname(__FILE__), 'commands')
+      files = Dir.entries(path).select { |f| f.include?('.rb') }
+      commands = {}
+      files.each do |f|
+        name = f[0..-4]
+        method = "Commands::#{name.capitalize}.run"
+        commands["/#{name}"] = method
+        commands["/#{name}@RichardStallman_bot"] = method
+      end
+      commands
+    end
 
-    def self.run(message)
+    def self.all
+      @all ||= commands
+    end
 
-
+    class << self
+      private :commands
     end
   end
 end
--- lib/stallman_bot/listener.rb
+++ lib/stallman_bot/listener.rb
@@ -1,5 +1,27 @@
+require 'stallman_bot/commands'
 
 module StallmanBot
   class Listener
+    def initalize
+      @listening = false
+    end
+
+    def listen(message, bot = nil)
+      if command?(message)
+        puts "command"
+        # send(Commands.all[cmd], {message, bot})
+      else
+        puts "Filter and reply" #if @listening
+      end
+    end
+
+    def command?(cmd)
+      Commands.all.key?(cmd)
+    end
+
+    def reply
+    end
+
+    private :command?, :reply
   end
 end
